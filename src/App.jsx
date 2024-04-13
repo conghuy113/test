483f8c02c179d7d497323596b981ed10a7dfb45a
abc <conghuy.fptu@gmail.com>
2024-04-13 20:16:18 +0700

    test git

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function DemoReact(){
+  return <h2>Huy đẹp troai</h2>;
+}
+
+export default DemoReact;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import DemoReact from './App.jsx'
+
+//single page application
+//client side rendering
+
+ReactDOM.createRoot(document.getElementById('root')).render(<DemoReact/>)
